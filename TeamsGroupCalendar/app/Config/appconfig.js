ddf1b9995a242cbd795d9ca18291f3f50d4aedf0
joe.io <dev@joe.io>
2021-03-11 07:24:30 -0500

    Initial TeamGroupCalendar Push

---
+++ TeamsGroupCalendar/app/Config/appconfig.js
@@ -0,0 +1,9 @@
+const getConfig = () => {
+   var config = {
+        clientId : "71db5de8-6b7d-437c-b973-0e13f81500e0",
+        redirectUri : "/TeamsGroupCalendar/app/silent-end.html",
+        authwindow :  "/TeamsGroupCalendar/app/auth.html",
+	hostRoot: "https://joeio.github.io",
+   };
+   return config;
+}
