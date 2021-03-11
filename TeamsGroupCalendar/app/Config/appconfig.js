724167d0e860d09566ab14f65d61eb2ec2c3578e
joe.io <dev@joe.io>
2021-03-11 07:33:40 -0500

    AppId Modification

--- TeamsGroupCalendar/app/Config/appconfig.js
+++ TeamsGroupCalendar/app/Config/appconfig.js
@@ -1,6 +1,6 @@
 const getConfig = () => {
    var config = {
-        clientId : "71db5de8-6b7d-437c-b973-0e13f81500e0",
+        clientId : "d26fce50-6f35-4267-9000-ee8d2d275153",
         redirectUri : "/TeamsGroupCalendar/app/silent-end.html",
         authwindow :  "/TeamsGroupCalendar/app/auth.html",
 	hostRoot: "https://joeio.github.io",
